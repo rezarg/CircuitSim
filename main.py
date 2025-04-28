9a086e91ec32e7c5839e91d375b82cf9b0050fc2
rezarg <squeenybenicheeny@gmail.com>
2025-04-28 07:20:10 -0700

    commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from pygame import *
 import pygame, time
-import logic
+
 from logic import Block, drawConnection, init as initLogic
 from data import encodeData, decodeData, init as initData
 
 pygame.init()
 pygame.font.init()
 
-window = display.set_mode((1280, 720))
-display.set_caption("Circuit Sim")
+window = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Circuit Sim")
 
-font = font.Font()
+font = pygame.font.Font()
 
 RUNNING = True
 TPS = 100
@@ -30,32 +29,32 @@ initData(blocks)
 
 while RUNNING:
 	for event in pygame.event.get():
-		if event.type == QUIT:
+		if event.type == pygame.QUIT:
 			encoded = encodeData()
 			open("save-backup.txt", "w+").write(encoded)
 			RUNNING = False
-		if event.type == KEYDOWN:
-			if event.key == K_w:
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
 				currentBlockType = (currentBlockType - 1) % len(blockTypes)
-			elif event.key == K_s:
+			elif event.key == pygame.K_s:
 				currentBlockType = (currentBlockType + 1) % len(blockTypes)
-			elif event.key == K_e:
+			elif event.key == pygame.K_e:
 				for logic in blocks:
 					if logic.x - 10 < mouseX < logic.x + 10 and logic.y - 10 < mouseY < logic.y + 10:
 						logic.value = not logic.value
 						break
-			elif event.key == K_i:
+			elif event.key == pygame.K_i:
 				inputStr = input("Enter block data: ")
 				decodeData(inputStr)
-			elif event.key == K_o:
+			elif event.key == pygame.K_o:
 				encodeData()
-			elif event.key == K_EQUALS or event.key == K_PLUS or event.key == K_KP_PLUS:
+			elif event.key == pygame.K_EQUALS or event.key == pygame.K_PLUS or event.key == pygame.K_KP_PLUS:
 				TPS += 5
 				if TPS > 1000: TPS = 1000
-			elif event.key == K_MINUS or event.key == K_KP_MINUS:
+			elif event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS:
 				TPS -= 5
 				if TPS < 5: TPS = 5
-		if event.type == MOUSEBUTTONDOWN:
+		if event.type == pygame.MOUSEBUTTONDOWN:
 			dragStartX, dragStartY = mouseX, mouseY
 			if event.button == 1:
 				for logic in blocks:
@@ -84,7 +83,7 @@ while RUNNING:
 					if logic.x - 10 < mouseX < logic.x + 10 and logic.y - 10 < mouseY < logic.y + 10:
 						connecting = True
 						break
-		if event.type == MOUSEBUTTONUP:
+		if event.type == pygame.MOUSEBUTTONUP:
 			if event.button == 3:
 				m2 = False
 				connecting = False
@@ -101,7 +100,7 @@ while RUNNING:
 				if start and end and start != end:
 					if end not in start.inputs:
 						end.inputs.append(start)
-		if event.type == MOUSEMOTION:
+		if event.type == pygame.MOUSEMOTION:
 			x, y = event.pos
 			mouseX = x // 20 * 20 + 10
 			mouseY = y // 20 * 20 + 10
@@ -129,14 +128,14 @@ while RUNNING:
 	for i in range(len(blockTypes)):
 		window.blit(font.render(blockTypes[i], True, (255, 255, 255)), (26, i*24+4))
 		if i == currentBlockType:
-			draw.rect(window, (255, 255, 255), (4, i*24+3, 20, 20))
+			pygame.draw.rect(window, (255, 255, 255), (4, i*24+3, 20, 20))
 		else:
-			draw.rect(window, (128, 128, 128), (4, i*24+4, 20, 20))
+			pygame.draw.rect(window, (128, 128, 128), (4, i*24+4, 20, 20))
 
 	text = font.render("TPS: " + str(TPS), True, (255, 255, 255))
 	window.blit(text, (window.get_width() - text.get_width() - 4, 4))
 
-	display.flip()
+	pygame.display.flip()
 
 # ➖🟩🟩🟩
 # ➖🟩🟩⬛
